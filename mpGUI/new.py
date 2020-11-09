d3db7238726223be0b95e814074c9c4a3fe3fefe
EgeISerbes <egeserbes50@gmail.com>
2020-11-09 04:28:43 +0300

    Update new.py

--- mpGUI/new.py
+++ mpGUI/new.py
@@ -4,28 +4,38 @@ from PyQt5.QtGui import *
 
 import  sys
 
-class Window(QWidget):
+class Window(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setUI()
+        self.show()
 
     def setUI(self):
         widget = QWidget()
-        gridLayout = QGridLayout()
-        
+        v_box = QVBoxLayout()
+        h_box = QHBoxLayout()
+        self.searchLine = QLineEdit()
+        button = QPushButton("Find package from PyPI")
+        #self.sonuc = QLabel()
+        v_box.addLayout(h_box)
+
+        h_box.addWidget(self.searchLine)
+        h_box.addWidget(button)
+        #v_box.addWidget(self.sonuc)
+
+
+        widget.setLayout(v_box)
+        self.setCentralWidget(widget)
 
 
 
-        widget.setLayout(gridLayout)
+
+
+"""class thread """
 
 
 
-
-class thread
-
-
-
-if __name__ == " main " :
+if __name__ == "__main__" :
     app = QApplication(sys.argv)
     window = Window()
     sys.exit(app.exec())
